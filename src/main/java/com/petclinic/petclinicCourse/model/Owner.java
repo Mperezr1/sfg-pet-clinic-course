aee329ff22834f7ec79aab5ed13fb164e0c8a374
Manuel Perez <manuel.perez@keyrus.com>
2022-07-05 18:13:40 -0500

    Adding POJO's for start. Closes #2.

---
+++ src/main/java/com/petclinic/petclinicCourse/model/Owner.java
@@ -0,0 +1,4 @@
+package com.petclinic.petclinicCourse.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/petclinic/petclinicCourse/model/Vet.java
@@ -0,0 +1,5 @@
+package com.petclinic.petclinicCourse.model;
+
+public class Vet extends Person{
+
+}
